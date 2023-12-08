d68974f5b854153ee3282422b2a41e5a9efa3f1f
Lagacy_code <miraglioenzo93@gmail.com>
2023-12-08 15:37:39 +0100

    feat(js) : accordion

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const carouselContainer = document.getElementById("carouselContainer");
 const featureSection = document.getElementById("featureSection");
 const navLiFeatures = featureSection.querySelectorAll("ul > li");
+const accordionontainer = document.getElementById("accordion");
 
 function activeFeature(target) {
   navLiFeatures.forEach((li) => {
@@ -11,4 +12,14 @@ function activeFeature(target) {
 navLiFeatures.forEach((li) => {
   li.addEventListener("click", () => activeFeature(li));
 });
+
+accordionontainer.querySelectorAll("article").forEach((article) => {
+  article.querySelector("h4").addEventListener("click", (e) => {
+    article.dataset.open = article.dataset.open === "false" ? "true" : "false";
+  });
+  article.querySelector("svg").addEventListener("click", (e) => {
+    article.dataset.open = article.dataset.open === "false" ? "true" : "false";
+  });
+});
+
 document.querySelector("form").addEventListener("submit", (e) => e.preventDefault());
